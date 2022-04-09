8c0cb270cb32c7b750f7e25d163dd70c6ae554b4
Pedro Henrique Martins Borges <88720549+PiedroCraft@users.noreply.github.com>
2022-04-09 12:52:14 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,63 @@
+from pytube import YouTube
+import youtube_dl
+import os
+from youtube_dl.utils import DownloadError 
+
+
+#https://www.youtube.com/watch?v=wdJvZnHdsvM
+url = input("URL do Youtube: ")
+request = "https://www.youtube.com/watch?"
+lenurl = len(url)
+
+#print(request in url and url[0:30] == request and lenurl > 29)
+if(request in url and url[0:30] == request and lenurl > 30):
+    type=input("\nTipo do Arquivo\nMP3 or MP4: ").upper()
+    
+    if type == "MP3":
+        try:
+            info = youtube_dl.YoutubeDL().extract_info(url=url, download=False)
+            if not os.path.exists("Downloads/mp3"):
+                os.makedirs("Downloads/mp3")
+            os.chdir("./")
+            os.chdir("./Downloads/mp3")
+            filename = f"{info['title']}.mp3"
+            options = {
+                'format' : 'bestaudio/best',
+                'keepvideo': False,
+                'outtmpl': filename,
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192'
+                }]
+            }
+            with youtube_dl.YoutubeDL(options) as ydl:
+                ydl.download([info['webpage_url']])
+            print(f"\n\nDownload da Música ({info['title']}) foi Concluido!\n")
+
+        except DownloadError:
+            print(f"\n\n    OPSS.... \n Houve Algum erro, Verifique sua URL ou sua Conexão a Internet e Tente Novamente!\n")
+
+    elif type == "MP4":
+        try:
+            yt = YouTube(url)
+            if not os.path.exists("Downloads/mp4"):
+                os.makedirs("Downloads/mp4")
+            os.chdir("./")
+            os.chdir("./Downloads/mp4")
+            result = {
+                "Título": yt.title,
+                "Número de views": yt.views,
+                "Tamanho do vídeo": yt.length,
+                "Avaliação do vídeo": yt.rating
+            }
+            ys = yt.streams.get_highest_resolution()
+            print("\nBaixando...")
+            ys.download()
+            print(f"\n\nDownload do Vídeo ({result['Título']}) foi Concluido!\n")
+        except:
+            print(f"\n\n    OPSS.... \n Houve Algum erro, Verifique sua URL ou sua Conexão a Internet e Tente Novamente!\n")
+    else:
+        print("\n\nEste tipo de arquivo não é suportada!\n")
+else:
+    print("\n\nA URL Informada é Invalida!\n")
